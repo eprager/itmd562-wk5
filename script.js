fe5b66f654fdd73b370b3412483adeac43d12b61
Emma Prager <eprager@hawk.iit.edu>
2019-09-26 17:45:58 -0500

    add doc

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function form() {
   //This will do things
   
 }
+
+$( "form" ).submit(function( event ) {
+  
+});
+
+function formsToObj(){
+  var forms = [];
+  $('form').each(function(i){
+      forms[i] = {};
+      $(this).children('input,textarea,select').each(function(){
+          forms[i][$(this).attr('name')] = $(this).val();
+      });
+  });
+  return forms;
+}
