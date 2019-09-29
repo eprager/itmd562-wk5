5f171394ce2b22e94b9c55cb4c163f0646525489
Emma Prager <eprager@hawk.iit.edu>
2019-09-29 12:21:20 -0500

    update form

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-$( "form" ).submit(function( event ) {
-  
-});
-
-function formsToObj(){
-  var forms = [];
-  $('form').each(function(i){
-      forms[i] = {};
-      $(this).children('input,textarea,select').each(function(){
-          forms[i][$(this).attr('name')] = $(this).val();
-      });
+$(document).ready(function(){
+  $("form").submit(function(){
+    alert("Submitted");
+    var x = $("form").serializeArray();
+    $.each(x, function(i, field){
+    $("#results").append(field.name + ":" + field.value + " ");
   });
-  return forms;
-}
+  });
+});
