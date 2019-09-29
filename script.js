f67b0a21fac71eb30e722eda2989f3b07b1a5b5f
Emma Prager <eprager@hawk.iit.edu>
2019-09-29 17:39:28 -0500

    save data with localstorage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var data = "";
+
 function Person(fname,sname,num){
   this.firstname = fname;
   this.lastname = sname; 
@@ -5,7 +7,7 @@ function Person(fname,sname,num){
   this.confirm = function(){
     alert(this.firstname + ' ' + this.lastname + "'s favorite number is " + this.number);
   }
-}  
+}
 
 $(document).ready(function(){
   $("#submitBtn").click(function(){        
@@ -14,8 +16,16 @@ $(document).ready(function(){
         document.getElementById('firstname').value, 
         document.getElementById('lastname').value,
         document.getElementById('number').value
-        );
-      p.confirm();
+      );
+      console.log(p);
+      var myJSON = JSON.stringify(p);
+      data = data + myJSON + "<br>";
+      window.localStorage.setItem('dataStorage', data);
+  });
+
+  $("#outputBtn").click(function(){  
+    var output = window.localStorage.getItem('dataStorage');
+    $("#results").append(output);
   });
 });
 
